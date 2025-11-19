0b24f49cda036cdbd64f2122295244a35d1b154d
yvantor <yvan.tortorella@gmail.com>
2025-11-19 08:02:21 +0100

    Simplify printf to its bare minimum. The runtime should be extended to initialize the UART after boot.

--- sw/tests/bare-metal/hostd/helloworld.c
+++ sw/tests/bare-metal/hostd/helloworld.c
@@ -22,6 +22,12 @@ int main(void) {
 
     // Init the HW
     car_init_start();
-    printf("Hi!\n");
+    // printf("Hi!\n");
+    char str[] = "Hi\n";
+    uint32_t rtc_freq = *reg32(&__base_regs, CHESHIRE_RTC_FREQ_REG_OFFSET);
+    uint64_t reset_freq = clint_get_core_freq(rtc_freq, 2500);
+    uart_init(&__base_uart, reset_freq, __BOOT_BAUDRATE);
+    uart_write_str(&__base_uart, str, sizeof(str) - 1);
+    uart_write_flush(&__base_uart);
     return 0;
 }
