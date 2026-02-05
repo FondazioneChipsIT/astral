f9bad32ce63aae61040f48e7f710333201e79b1a
Gabriele Lodi <gabriele.lodi93@gmail.com>
2026-02-05 15:50:59 +0100

    [REGMAP] Restore security island default config with axi_isolate=1 and clk_en=0

--- hw/regs/carfield_reg_top.sv
+++ hw/regs/carfield_reg_top.sv
@@ -598,7 +598,7 @@ module carfield_reg_top #(
   prim_subreg #(
     .DW      (1),
     .SWACCESS("RW"),
-    .RESVAL  (1'h0)
+    .RESVAL  (1'h1)
   ) u_security_island_isolate (
     .clk_i   (clk_i    ),
     .rst_ni  (rst_ni  ),
@@ -922,7 +922,7 @@ module carfield_reg_top #(
   prim_subreg #(
     .DW      (1),
     .SWACCESS("RW"),
-    .RESVAL  (1'h1)
+    .RESVAL  (1'h0)
   ) u_security_island_clk_en (
     .clk_i   (clk_i    ),
     .rst_ni  (rst_ni  ),
