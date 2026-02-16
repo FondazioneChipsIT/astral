53c17b4172671bc033c521f471fa18b8cc28cdbc
Luigi Ghionda <luigi.ghio.98@gmail.com>
2026-02-17 00:14:09 +0100

    [SW] Add Secure Domain tests compilation flow (WIP)

---
+++ env/secd-env.sh
@@ -0,0 +1,8 @@
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Solderpad Hardware License, Version 0.51, see LICENSE for details.
+# SPDX-License-Identifier: SHL-0.51
+#
+
+# set up environment variables for rtl simulation
+ROOTD=$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")/.." && pwd)
+[[ -d "$ROOTD/opentitan/sw/tests/pulp-runtime" ]] && source "$ROOTD//opentitan/sw/tests/pulp-runtime/configs/opentitan-cluster.sh"
