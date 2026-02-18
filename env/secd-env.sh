5b3514854aeff7203a05c0568b001fbaaf5e4741
Luigi Ghionda <luigi.ghio.98@gmail.com>
2026-02-18 15:43:07 +0100

    [SW] Enable printing from Security Island on host UART

--- env/secd-env.sh
+++ env/secd-env.sh
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: SHL-0.51
 #
 
+export NO_STANDALONE=1
+
 # set up environment variables for rtl simulation
 ROOTD=$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")/.." && pwd)
 [[ -d "$ROOTD/opentitan/sw/tests/pulp-runtime" ]] && source "$ROOTD//opentitan/sw/tests/pulp-runtime/configs/opentitan-cluster.sh"
