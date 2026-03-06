b3507d360a33f63f8f9c190f38644a213a29b6b0
Luigi Ghionda <luigi.ghio.98@gmail.com>
2026-03-07 00:14:39 +0100

    [Makefile] Use venv for auto-generated stuff
    * Force setuptools==53.0.0 to avoid error with some python versions

--- carfield.mk
+++ carfield.mk
@@ -142,7 +142,7 @@ AXIRT_NUM_SUBS := 2
 # Virtual environment for python scripts #
 ##########################################
 
-VENVDIR?=$(WORKDIR)/.venv
+VENVDIR?=$(CAR_ROOT)/.venv
 REQUIREMENTS_TXT?=$(wildcard requirements.txt)
 include $(CAR_ROOT)/utils/venv.mk
 
@@ -181,7 +181,8 @@ include $(CAR_SW_DIR)/sw.mk
 ## Build the host domain (Cheshire) SW libraries and generates an archive (`libcheshire.a`)
 ## available for Carfield as static library at link time.
 CHS_SW_FLAGS += -Wno-int-conversion -Wno-implicit-function-declaration -Wno-incompatible-pointer-types -Wno-implicit-int
-chs-sw-build: chs-sw-all
+chs-sw-build: | venv
+	. "$(VENV)/activate" && $(MAKE) chs-sw-all
 
 .PHONY: car-sw-build
 ## Builds carfield application SW and specific libraries. It links against `libcheshire.a`.
@@ -308,12 +309,12 @@ spatzd-hw-init:
 ## Generate Cheshire HW. This target has a prerequisite, i.e. the PLIC and serial link
 ## configurations must be chosen before generating the hardware.
 .PHONY: chs-hw-init
-chs-hw-init: update_plic update_serial_link
-	$(MAKE) -B chs-hw-all
+chs-hw-init: update_plic update_serial_link | venv
+	. "$(VENV)/activate" && $(MAKE) -B chs-hw-all
 
 .PHONY: idma-hw-init
-idma-hw-init:
-	$(MAKE) -C $(shell bender path idma) idma_hw_all
+idma-hw-init: | venv
+	. "$(VENV)/activate" && $(MAKE) -C $(shell bender path idma) idma_hw_all
 
 ##############
 # Simulation #
