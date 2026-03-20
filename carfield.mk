71fb78ebf8e527395604094d50832665e6a540c7
yvantor <yvan.tortorella@gmail.com>
2026-03-20 14:57:01 +0100

    Update tech commit.

--- carfield.mk
+++ carfield.mk
@@ -413,7 +413,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@gitlab.chips.it:digitalresearchline/scar-v/$(TECHNOLOGY).git
 # no commit by default, change during development
-tech-commit := 0bdf92dc47b72c64ea8bbd15a21e24089c48ef32 # branch: main
+tech-commit := ba591de81cb6d9f7b01759fa96f7d547f49f81e4 # branch: main
 
 tech-clone:
 	git clone $(tech-repo) $(CAR_TECH_DIR)
