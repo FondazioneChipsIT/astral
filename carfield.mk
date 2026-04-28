5af1714dde2b0a712f49f6273acbebb513bf1616
Andiruz <andiruz@gmail.com>
2026-04-28 13:26:49 +0200

    [FIX] update tech commit

--- carfield.mk
+++ carfield.mk
@@ -430,7 +430,7 @@ car-check-litmus-tests: $(LITMUS_WORK_DIR)/litmus.log
 ##############
 tech-repo := git@gitlab.chips.it:digitalresearchline/scar-v/$(TECHNOLOGY).git
 # no commit by default, change during development
-tech-commit := 876000f2ba8186b2cd09529345c13e52e6549104 # branch: main
+tech-commit := adr/tapeout # branch: main
 
 tech-clone:
 	git clone $(tech-repo) $(CAR_TECH_DIR)
